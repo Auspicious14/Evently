0d5d93036d2c9b631601962d93e8ab945cee306c
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-10-08 17:31:11 +0100

    Update filter-event.dto.ts

--- src/events/dto/filter-event.dto.ts
+++ src/events/dto/filter-event.dto.ts
@@ -4,9 +4,13 @@ import { PaginationQueryDto } from '../../common/dto/pagination-query.dto';
 export class FilterEventDto extends PaginationQueryDto {
   @IsString()
   @IsOptional()
+  title?: string
+  
+  @IsString()
+  @IsOptional()
   location?: string;
 
-  @IsEnum(['AI', 'Fintech', 'Startup', 'Coding'])
+  @IsEnum(['AI', 'Fintech', 'Startup', 'Coding', 'Hardware', 'Design', 'Marketing', 'Cybersecurity', 'Virtual'])
   @IsOptional()
   category?: string;
 
